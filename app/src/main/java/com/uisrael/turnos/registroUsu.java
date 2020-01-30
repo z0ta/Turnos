6b9096ddabe5179ff64a98a5bc6df7451d10bffe
z0ta <brs14101992>
2020-01-30 11:53:04 -0500

    Login

--- app/src/main/java/com/uisrael/turnos/registroUsu.java
+++ app/src/main/java/com/uisrael/turnos/registroUsu.java
@@ -3,15 +3,12 @@ package com.uisrael.turnos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class registroUsu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registro_usu);
     }
 }
